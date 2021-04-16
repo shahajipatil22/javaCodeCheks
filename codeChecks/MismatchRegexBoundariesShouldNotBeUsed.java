06ed93f8cc48fb27e8b019c22ffa205e3ba634cd
GhatnekarPreeti <preeti.ghatnekar@embold.io>
2021-04-16 16:03:20 +0530

    Revert "added code issue for FN"
    This reverts commit 65587d8f9ddf3923ec70f72a332f9cec0026579b.

--- codeChecks/MismatchRegexBoundariesShouldNotBeUsed.java
+++ codeChecks/MismatchRegexBoundariesShouldNotBeUsed.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 public class MismatchRegexBoundariesShouldNotBeUsed {
 	// EMB-ISSUE: CodeIssueNames.MISMATCH_REGEX_BOUNDARIES_SHOULD_NOT_BE_USED
 	Pattern pattern = Pattern.compile("$[a-z]+^");
-	java.util.regex.Pattern.compile("$[a-z]+^");
 	Matcher matcher = pattern.matcher("");
 	// EMB-ISSUE:
 	// CodeIssueNames.MISMATCH_REGEX_BOUNDARIES_SHOULD_NOT_BE_USED/no-detect
